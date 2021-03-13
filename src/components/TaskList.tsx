a2a8aaa8d0fd058489ad96388a6c283b75d9d2bd
pdrolucasz <pedro.lucas4431@gmail.com>
2021-03-13 19:33:48 -0300

    componente de tasklist finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldTasks => [...oldTasks, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggledTask = tasks.map(task => {
+      if(task.id == id) return {...task, isComplete: !task.isComplete}
+      return task
+    })
+
+    setTasks(toggledTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
   }
 
   return (
@@ -33,7 +50,7 @@ export function TaskList() {
 
         <div className="input-group">
           <input 
-            type="text" 
+            type="text"
             placeholder="Adicionar novo todo" 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
